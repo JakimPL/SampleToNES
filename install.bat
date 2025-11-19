0d4bafcda0cec8b3924d13652044c5363ba70220
JakimPL <jakimpl@gmail.com>
2025-11-19 19:51:38 +0100

    Fixed: installation script

--- install.bat
+++ install.bat
@@ -29,10 +29,10 @@ pip install .
 pip install pyinstaller
 
 echo Building executable...
-pyinstaller --name sampletones \
-    --onefile \
-    --distpath . \
-    --icon src\sampletones\icons\sampletones.ico \
+pyinstaller --name sampletones ^
+    --onefile ^
+    --distpath . ^
+    --icon src\sampletones\icons\sampletones.ico ^
     --add-data "src\sampletones\icons;sampletones\icons" src\sampletones\__main__.py
 
 if exist sampletones.exe (
