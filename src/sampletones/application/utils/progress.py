dc699b88bf4a9cc2b917da65072192d213e59527
JakimPL <jakimpl@gmail.com>
2025-11-22 11:54:38 +0100

    Improved: system progress

--- src/sampletones/application/utils/progress.py
+++ src/sampletones/application/utils/progress.py
@@ -10,7 +10,7 @@ class SystemProgress:
         self.value = 0
         self.maximum = 0
 
-    def initialize(self):
+    def initialize(self) -> None:
         if not sys.platform.startswith("win"):
             return
 
@@ -26,24 +26,42 @@ class SystemProgress:
             self.progress = Progress(hwnd)
         except (ModuleNotFoundError, ImportError) as exception:
             logger.warning(f"Could not import Windows progress bar dependencies: {exception}")
+        except Exception as exception:  # pylint: disable=broad-except
+            self._fallback(exception)
 
-    def error(self):
+    def error(self) -> None:
         if self.progress is not None:
-            self.progress.set_progress_type(15)
+            try:
+                self.progress.set_progress_type(15)
+            except Exception as exception:  # pylint: disable=broad-except
+                self._fallback(exception)
 
-    def start(self, maximum: int):
-        self.value = 0
-        self.maximum = maximum
+    def start(self, maximum: int) -> None:
         if self.progress is not None:
-            self.progress.set_progress_type(0)
-            self.progress.set_progress(0, self.maximum)
+            self.value = 0
+            self.maximum = maximum
+            try:
+                self.progress.set_progress_type(0)
+                self.progress.set_progress(0, self.maximum)
+            except Exception as exception:  # pylint: disable=broad-except
+                self._fallback(exception)
 
-    def set(self, value: int, maximum: int):
-        self.value = min(maximum, max(self.value, value))
-        self.maximum = maximum
+    def set(self, value: int, maximum: int) -> None:
         if self.progress is not None:
-            self.progress.set_progress(self.value, self.maximum)
+            self.value = min(maximum, max(self.value, value))
+            self.maximum = maximum
+            try:
+                self.progress.set_progress(self.value, self.maximum)
+            except Exception as exception:  # pylint: disable=broad-except
+                self._fallback(exception)
 
-    def clear(self):
+    def clear(self) -> None:
         if self.progress is not None:
-            self.progress.set_progress(0, 1)
+            try:
+                self.progress.set_progress(0, 1)
+            except Exception as exception:  # pylint: disable=broad-except
+                self._fallback(exception)
+
+    def _fallback(self, exception: Exception) -> None:
+        logger.warning(f"Could not set Windows progress bar: {exception}")
+        self.progress = None
