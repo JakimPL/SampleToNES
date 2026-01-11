699f81503d95d1d5e5da9f8cac194c3b5831b4a2
JakimPL <jakimpl@gmail.com>
2026-01-11 20:28:02 +0100

    Added: removing items

--- src/sampletones/utils/collections/indexed.py
+++ src/sampletones/utils/collections/indexed.py
@@ -24,7 +24,9 @@ class IndexedCollection(Generic[T]):
         raise TypeError(f"Index must be an int or str, got {type(key)}")
 
     def __repr__(self) -> str:
-        return f"IndexedCollection({list(self._items.values())})"
+        return f"IndexedCollection({[
+            self._items[self._order.backward(item)] for item in range(len(self._order))
+        ]})"
 
     def get(self, key: Union[int, str]) -> Optional[T]:
         if isinstance(key, int):
@@ -38,18 +40,17 @@ class IndexedCollection(Generic[T]):
 
         raise TypeError(f"Index must be an int or str, got {type(key)}")
 
-    def add(self, item: T) -> int:
-        item_hash = hash_model(item)
-        if item_hash in self._items:
-            return self._order.forward(item_hash)
-
-        index = len(self._order)
-        return self._set(index, item_hash, item)
+    def add(self, item: T) -> None:
+        self.insert(len(self._order), item)
 
     def _set(self, index: int, item_hash: str, item: T) -> int:
         self._items[item_hash] = item
         self._order[item_hash] = index
         return index
+
+    def _unset(self, index: int) -> None:
+        item = self._order.pop_backward(index)
+        del self._items[item]
 
     def insert(self, index: int, item: T) -> None:
         if index < 0 or index > len(self._order):
@@ -59,17 +60,22 @@ class IndexedCollection(Generic[T]):
         if item_hash in self._items:
             return
 
-        for i in range(len(self._order), index, -1):
-            self._order.remap_backward(i - 1, i)
-
+        self._reindex(index, add=True)
         self._set(index, item_hash, item)
+
+    def remove(self, index: int) -> None:
+        self._unset(index)
+        self._reindex(index, add=False)
+
+    def _reindex(self, index: int, add: bool = True) -> None:
+        for i in range(len(self._order), index, -1):
+            if add:
+                self._order.remap_backward(i - 1, i)
+            else:
+                self._order.remap_backward(i, i - 1)
 
     def order(self, key: str) -> Optional[int]:
         try:
             self._order.forward(key)
         except KeyError:
             pass
-
-    def remove(self, index: int) -> None:
-        if index in self._items:
-            del self._items[index]
