7fa410b949fd32603b1698a43ce03af702b83d5b
JakimPL <jakimpl@gmail.com>
2026-06-08 10:27:33 +0200

    Updated: dependencies

--- scripts/linux/build/dependencies.sh
+++ scripts/linux/build/dependencies.sh
@@ -6,7 +6,7 @@ echo "Install additional system dependencies? (y/N)"
 read -r INSTALL_SYS_DEPS
 if [[ "$INSTALL_SYS_DEPS" == "y" || "$INSTALL_SYS_DEPS" == "Y" ]]; then
     sudo apt-get update
-    sudo apt-get install -y python3-tk tk-dev tcl-dev libportaudio2 libasound-dev
+    sudo apt-get install -y python3-tk tk-dev tcl-dev libportaudio2 libasound-dev libpulse-dev portaudio19-dev
     echo "Additional system dependencies installed."
 else
     echo "Warning: Skipping system dependencies installation may lead to build failures."
